61cd557adb23d24e971dd5512bea47ee6119ad33
kjaffel <khawla.jaffel@cern.ch>
2021-07-27 19:23:56 +0200

    add random seed flag

--- gridpackTolheToNanoGen.sh
+++ gridpackTolheToNanoGen.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-if [ $# != 3 ]; then
-  echo "Need exactly three arguments: fragment, and output file and the gridpack path"
+if [ $# != 4 ]; then
+  echo "Need exactly four arguments: fragment, and output file and the gridpack path and randomseed"
   exit 1
 fi
 if [ -z "${CMSSW_BASE}" ]; then
@@ -13,9 +13,10 @@ output=$(realpath "${2}")
 startdir=$(pwd)
 tmpdir=$(mktemp -d)
 pushd "${tmpdir}"
-nEvents=10000
+nEvents=2000
+randomseed=${4}
 
-cmsDriver.py "${fragment}" --fileout "file:${output}" --mc --eventcontent NANOAODSIM --datatier NANOAOD --conditions auto:mc --step LHE,GEN,NANOGEN -n "${nEvents}" --customise_commands "process.nanoAOD_step.remove(process.rivetProducerHTXS); process.nanoAOD_step.remove(process.HTXSCategoryTable) ;process.externalLHEProducer.args = cms.vstring('${3}')"
+cmsDriver.py "${fragment}" --fileout "file:${output}" --mc --eventcontent NANOAODSIM --datatier NANOAOD --conditions auto:mc --step LHE,GEN,NANOGEN -n "${nEvents}" --customise_commands "process.nanoAOD_step.remove(process.rivetProducerHTXS); process.nanoAOD_step.remove(process.HTXSCategoryTable) ;process.externalLHEProducer.args = cms.vstring('${3}'); process.RandomNumberGeneratorService.externalLHEProducer.initialSeed=${randomseed}"
 
 popd
 #if [ -d "${tmpdir}" ]; then
